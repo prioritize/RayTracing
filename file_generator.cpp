969144c8dca3bb008222f9c34d86ceb158575eef
Jared Leach <leach.jared@gmail.com>
2018-09-20 06:29:58 -0700

    background gradient working

--- file_generator.cpp
+++ file_generator.cpp
@@ -1,21 +1,61 @@
 #include <iostream>
-#include "vec3.h"
+#include <fstream>
+#include "ray.h"
+// #include "vec3.h"
 
+vec3 color(const Ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5*(unit_direction.y() + 1.0);
+    std::cout << "unit_direction.y(): " << unit_direction.y() << std::endl;
+    std::cout << "Calculated t: " << t << std::endl;
+    vec3 out = (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
+    std::cout << "r: " << out[0] << "g: " << out[1] << "b: " << out[2];
+    return out;
+    
+}
 int main(){
-    int width = 1920,  height = 1080;
-    std::cout << "P3" << std::endl;
-    std::cout << width << " " << height << std::endl;
-    std::cout << "255" << std::endl;
-    for(int y = 0; y < height; y++){
-        for(int x = 0; x < width; x++){
-            float r = float(x)/float(width);
-            float g = float(y)/float(height);
-            float b = 0.2;
-            int ir = int(255.99*r);
-            int ig = int(255.99*g);
-            int ib = int(255.99*b);
-            std::cout << ir << " " << ig << " " << ib << std::endl;
+    std::fstream write_file;
+    write_file.open("test.ppm", std::ios::out | std::ios::trunc);
+    // Generate the header information for the PPM file.
+    int width = 400,  height = 200;
+    write_file << "P3" << std::endl;
+    write_file << width << " " << height << std::endl;
+    write_file << "255" << std::endl;
+    // End header information
+    
+    // Camera setup information
+    vec3 lower_left_corner(-width/2, -height/2, -height/2);
+    vec3 horizontal(width/2, 0.0, 0.0);
+    vec3 vertical(0.0, width/2, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
+    for(int j = height - 1; j >= 0; j--){
+        for(int i = 0; i < width; i++){
+            float u = float(i)/float(width);
+            float v = float(j)/float(height);
+            // float b = 0.2;
+            Ray ray(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 test = u*horizontal;
+            std::cout << "test values: " << test[0] << " " << test[1] << " " << test[2];            
+            if(ray.B[2] == 0){
+                std::cout << "You're fucked" << std::endl;
+            }
+            std::cout << ray.B[0] << ray.B[1] << ray.B[2] << std::endl;
+            std::cout << "(x: " << i << ", " << "y: "  << j << ")" << std::endl;
+            vec3 col = color(ray);
+            std::cout << "blue: "<< int(255.99*col[2]) << std::endl; 
+            int ir = int(255.99*col[0]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
+            write_file << ir << " " << ig << " " << ib << "\n";
+            // write_file << ib << std::endl;
+            // if(ib > 255){
+            //     std::cout << "ib greater than 1"<< std::endl;
+            //     std::cout << col[2] << std::endl;
+            // }
+        std::cout << std::endl;
         }
     }
+    write_file.close();
     
 }
--- ray.h
+++ ray.h
@@ -3,19 +3,19 @@
 #include "vec3.h"
 class Ray{
     public:
-    Ray() {}
-    Ray(const vec3& a, const vec3& b) {A = a; B = b;}
-    vec3 origin() const {
-        return A;
-    }
-    vec3 direction() const {
-        return B;
-    }
-    vec3 point_at_parameter(float t) const{
-        return A + t*B;
-    }
-    vec3 A;
-    vec3 B;
-}
+        Ray() {}
+        Ray(const vec3& a, const vec3& b) {A = a; B = b;}
+        vec3 origin() const {
+            return A;
+        }
+        vec3 direction() const {
+            return B;
+        }
+        vec3 point_at_parameter(float t) const{
+            return A + t*B;
+        }
+        vec3 A;
+        vec3 B;
+};
 
 #endif
