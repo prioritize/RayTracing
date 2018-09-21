37aeb72fdffcfeae83399a4d718e3bb4f94b8d4d
Jared Leach <leach.jared@gmail.com>
2018-09-20 23:57:21 -0700

    end of day

---
+++ hitable.h
@@ -0,0 +1,16 @@
+#ifndef HITABLEH
+#define HITABLEH
+
+#include "ray.h"
+
+struct HitRecord{
+    float t;
+    vec3 p;
+    vec3 normal;
+};
+
+class Hitable{
+    public:
+        virtual bool hit(const Ray& r, float t_min, float t_max, HitRecord& rec) const = 0;
+};
+#endif
