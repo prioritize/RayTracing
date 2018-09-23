2ff4ae989886333b7b303138dae748ceb1aa919d
prioritize <leach.jared@gmail.com>
2018-09-23 01:54:03 -0700

    Started anti-aliasing

--- file_generator.cpp
+++ file_generator.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
+// #include <stdlib.h>
 #include "float.h"
 #include "sphere.h"
 #include "hitablelist.h"
+#include "camera.h"
 
 #ifndef MAXFLOAT
 #define MAXFLOAT FLT_MAX
@@ -25,28 +27,30 @@ int main(){
     // Generate the header information for the PPM file.
     int width = 200;
     int height = 100;
+    int ns = 100;
     write_file << "P3" << std::endl << width << " " << height << " " << std::endl << "255" << std::endl;
     // write_file << width << " " << height << std::endl; 
     // write_file << "255" << std::endl;
     // End header information
     
     // Camera setup information
-    Vector3 lower_left_corner(-2.0, -1.0, -1.0);
-    Vector3 horizontal(4.0, 0.0, 0.0);
-    Vector3 vertical(0.0, 2.0, 0.0);
-    Vector3 origin(0.0, 0.0, 0);
     Hitable *list[2];
     list[0] = new Sphere(Vector3(0, 0, -1), 0.5);
     list[1] = new Sphere(Vector3(0, -100.5, -1), 100);
     Hitable *world = new HitableList(list, 2);
-
+    Camera cam;
     for(int j = height - 1; j >= 0; j--){
         for(int i = 0; i < width; i++){
-            float u = float(i)/float(width);
-            float v = float(j)/float(height);
-            Ray ray(origin, lower_left_corner + u*horizontal + v*vertical);
-            Vector3 p = ray.point_at_parameter(2.0);
-            Vector3 col = color(ray, world);
+            Vector3 col(0,0,0);
+            for(int s = 0; s < ns; s++){
+                float u = float(i + drand48())/float(width);
+                float v = float(j + drand48())/float(height);
+                Ray ray = cam.GetRay(u, v);
+                Vector3 p = ray.point_at_parameter(2.0);
+                col += color(ray, world);
+
+            }
+            col /= float(ns);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
